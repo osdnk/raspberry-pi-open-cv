54c373031d43a78f9a7de632f027e26743a546cf
osdnk <micosa97@gmail.com>
2018-11-12 23:57:53 +0100

    Minor fix

--- video_cap/videocap.py
+++ video_cap/videocap.py
@@ -40,14 +40,30 @@ class VideoCap:
     def get_arguments(self):
         arguments = self.functions[self.func_name][1:]
         if arguments.__len__() != 0:
-            print("Input user defined function arguments :)")
+            print("Input user defined function arguments")
             for arg in arguments:
                 self.args.append(int(input("Please, input " + str(arg) + " parameter value: ")))
 
-    def _none_filter(self, frame):
+    """
+    Dictionary of available video effects.
+        @key: function name used in arg_parser
+        @value: list of [class function name, user-defined arguments]
+    """
+    functions = {
+        'face-recognition': ['face_detection'],
+        'basic-threshold': ['threshhold_filter', 'threshold'],
+        'gauss-threshold': ['threshold_gauss', 'block_size', 'c'],
+        'sobel-det': ['sobel_edge_det', 'x derivative degree', 'y derivative degree'],
+        'laplacian-det': ['laplacian_edge_detecion'],
+        'canny-edge-det': ['canny_edge_det', 'min', 'max'],
+        'none': ['none_filter'],
+        'cartoon': ['cartoon_filter']
+    }
+
+    def none_filter(self, frame):
         return frame
 
-    def _threshold_gauss(self,
+    def threshold_gauss(self,
                          frame,
                          block_size,
                          c,
@@ -60,39 +76,37 @@ class VideoCap:
         matrix_fit = np.reshape(gauss, gauss.shape + (1,))
         return matrix_fit
 
-    def _threshhold_filter(self,
-                           frame,
-                           threshold,
-                           max_value=255,
-                           threshold_type=cv2.THRESH_BINARY):
+    def threshhold_filter(self,
+                          frame,
+                          threshold,
+                          max_value=255,
+                          threshold_type=cv2.THRESH_BINARY):
 
         _, threshold_frame = cv2.threshold(frame, threshold, max_value, threshold_type)
         return threshold_frame
 
-    def _sobel_edge_det(self, frame, x, y):
+    def sobel_edge_det(self, frame, x, y):
         gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         sobel = cv2.Sobel(gray_scale, cv2.CV_64F, x, y, ksize=5)
         matrix_fit = np.reshape(sobel, sobel.shape + (1,))
         return matrix_fit
 
-    def _laplacian_edge_detecion(self, frame):
+    def laplacian_edge_detecion(self, frame):
 
         return cv2.Laplacian(frame, cv2.CV_64F)
 
-    def _canny_edge_det(self, frame, min, max):
-
+    def canny_edge_det(self, frame, min, max):
         canny = cv2.Canny(frame, min, max)
         return np.reshape(canny, canny.shape + (1,))
 
-    def _face_detection(self, frame):
-
+    def face_detection(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = self.face_haar_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         return frame
 
-    def _cartoon_filter(self, frame, downscale=2, bilateral_steps=5):
+    def cartoon_filter(self, frame, downscale=2, bilateral_steps=5):
         for _ in range(downscale):
             frame = cv2.pyrDown(frame)
         for _ in range(bilateral_steps):
@@ -103,19 +117,3 @@ class VideoCap:
         edge = cv2.adaptiveThreshold(cv2.medianBlur(gray_scale, 7), 255,
                                      cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
         return cv2.bitwise_and(frame, cv2.cvtColor(edge, cv2.COLOR_GRAY2RGB))
-
-    """
-    Dictionary of available video effects.
-        @key: function name used in arg_parser
-        @value: list of [class function name, user-defined arguments]
-    """
-    functions = {
-        'face-recognition': ['_face_detection'],
-        'basic-threshold': ['_threshhold_filter', 'threshold'],
-        'gauss-threshold': ['_threshold_gauss', 'block_size', 'c'],
-        'sobel-det': ['_sobel_edge_det', 'x derivative degree', 'y derivative degree'],
-        'laplacian-det': ['_laplacian_edge_detecion'],
-        'canny-edge-det': ['_canny_edge_det', 'min', 'max'],
-        'none': ['_none_filter'],
-        'cartoon': ['_cartoon_filter']
-    }
