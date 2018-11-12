45a1239fa91a36be976cab6c85c528e82f05e125
osdnk <micosa97@gmail.com>
2018-11-12 21:35:52 +0100

    Change max args

--- video_cap/main.py
+++ video_cap/main.py
@@ -7,11 +7,11 @@ parser = argparse.ArgumentParser('Video capture in python using opencv lib')
 parser.add_argument('--fun', type=str,
                     choices=available_funcs,
                     help="Available functions: {0}".format(available_funcs), required=True, metavar="FUNC_NAME")
-parser.add_argument('-x', type=int, nargs=2, default=[0, 0],
+parser.add_argument('-x', type=int, nargs=2, default=[0, 10000],
                     help="Region of interest for filter on X axis [-x startcoord endcoord]",
                     metavar='X_RESOLUTIONS'
                     )
-parser.add_argument('-y', type=int, nargs=2, default=[0, 0],
+parser.add_argument('-y', type=int, nargs=2, default=[0, 10000],
                     help="Region of interest for filter on Y axis [-y startcoord endcoord]",
                     metavar='Y_RESOLUTIONS')
 
