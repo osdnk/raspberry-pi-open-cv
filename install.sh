fb5a0857aa1e24397be158876a1b71a46c4306d5
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-11-13 17:02:47 +0100

    Add -y to install scripts

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ dphys-swapfile setup
 dphys-swapfile swapon
 apt-get update && \
 apt-get upgrade && \
-apt-get install build-essential cmake pkg-config && \
-apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev && \
-apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev && \
-apt-get install libxvidcore-dev libx264-dev && \
-apt-get install libgtk2.0-dev libgtk-3-dev && \
-apt-get install libatlas-base-dev gfortran && \
-apt-get install python3-dev
+apt-get install -y build-essential cmake pkg-config && \
+apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev && \
+apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev && \
+apt-get install -y libxvidcore-dev libx264-dev && \
+apt-get install -y libgtk2.0-dev libgtk-3-dev && \
+apt-get install -y libatlas-base-dev gfortran && \
+apt-get install -y python3-dev
 wget -O opencv.zip "https://github.com/Itseez/opencv/archive/$VERSION.zip"
 unzip opencv.zip
 wget -O opencv_contrib.zip "https://github.com/Itseez/opencv_contrib/archive/$VERSION.zip"
