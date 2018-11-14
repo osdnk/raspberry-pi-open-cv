8cd27919099c480e83b8c3bc1752362d88d0201c
ajopek <arjopek@gmail.com>
2018-11-14 23:27:09 +0100

    Add imshow for debug

--- camera_server/camera_task.py
+++ camera_server/camera_task.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import datetime
+import time
 
 _PATH = '../video_cap/resources/trained_models/haarcascade_frontalface_default.xml'
 
@@ -16,10 +18,15 @@ while True:
         face_haar_cascade = cv2.CascadeClassifier(_PATH)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_haar_cascade.detectMultiScale(gray, 1.3, 5)
+        detected = False
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            detected = True
+        if detected:
             print("Face detected!!!")
-        #cv2.imwrite("static/images/snap.png", frame)
+            ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+            cv2.imwrite("static/images/face_{timestmp}.png".format(timestmp=ts), frame)
+            detected = False
         cv2.imshow('frame', frame)
     except cv2.error:
         print("Improperly defined resolution bounds or parameter values")
