f023ac6783620d2a4ac87210b9565914cbc3d978
ajopek <arjopek@gmail.com>
2018-11-16 01:05:52 +0100

    Add email sending and basic config file

--- camera_server/camera_task.py
+++ camera_server/camera_task.py
@@ -2,10 +2,47 @@ import numpy as np
 import cv2
 import datetime
 import time
+import smtplib
+import configparser
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
 
 _PATH = '../video_cap/resources/trained_models/haarcascade_frontalface_default.xml'
+config_path = 'config.ini'
 
 
+# Read config file
+config = configparser.ConfigParser()
+config.read_file(config_path)
+
+# Initialize email sending
+server_addr = config.get('email', 'server')
+port = config.get('email', 'port')
+username = config.get('email', 'username')
+password = config.get('email', 'password')
+fromaddr = config.get('email', 'fromaddr')
+toaddr = config.get('email', 'toaddr')
+
+def send_notification(img_name):
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "SOMEBODY JUST STOLE YOUR CARROTS"
+
+    body = "Somebody just tried to steal your carrots. Pics or it didn't happen, you say? I attached them."
+    msg.attach(MIMEText(body, 'plain'))
+    img_data = open('static/images/{img_name}.png'.format(img_name=img_name), 'rb').read()
+    msg.attach(MIMEImage(img_data, name=img_name))
+
+    text = msg.as_string()
+    server = smtplib.SMTP(server_addr, port)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(username, password)
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
 # Initialize camera
 cap = cv2.VideoCapture(0)
 while True:
@@ -26,6 +63,7 @@ while True:
             print("Face detected!!!")
             ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
             cv2.imwrite("static/images/face_{timestmp}.png".format(timestmp=ts), frame)
+            send_notification("face_{timestmp}.png".format(timestmp=ts))
             detected = False
         cv2.imshow('frame', frame)
     except cv2.error:
